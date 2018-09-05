a9ce1ecbf813dc28877355781f9c6230d8bb16ef
EC2 Default User <ec2-user@ip-172-31-8-26.ec2.internal>
2018-09-05 05:21:58 +0000

    Implement basic login

--- spec/features/users_login_spec.rb
+++ spec/features/users_login_spec.rb
@@ -11,13 +11,16 @@ RSpec.feature "UsersLogin", type: :feature do
     expect(page).to_not have_content "Invalid email/password combination"
   end
   
-  scenario "user can log in with valid information" do
+  scenario "user can log in and log out with valid information" do
     @user = create(:user)
     visit login_path
     fill_in "Email", with: @user.email
     fill_in "Password", with: 'password'
     click_button "Log in"
     expect(page).to have_content @user.name
+    expect(page).to have_content("Log out")    
+    click_link "Log out"
+    expect(page).to have_content("Welcome to the Sample App")     
   end
   
 end
